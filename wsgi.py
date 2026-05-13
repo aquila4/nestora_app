72d10815cc2eb78426f4a76f8d4e8ada1347a472
Aquila <aquilaaregbesola@gmail.com>
2026-05-13 09:36:23 +0100

    app update

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,9 @@ eventlet.monkey_patch()
 from app import create_app, socketio
 
 app = create_app()
+socketio.init_app(app)
+
+application = app  # IMPORTANT for gunicorn
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, debug=True)
